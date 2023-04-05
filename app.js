a3e8d33e4dab9334d7b443e91267177a84f26089
Nikola Mitic <nmitic304@gmail.com>
2023-04-05 23:34:18 +0200

    Nikola Mitic

--- app.js
+++ app.js
@@ -7,20 +7,26 @@ const app = express();
 //Povezivanje baze 
 const connectDB = require('./db/connect');
 
+//routers
+const productRouter = require('./routes/productRoutes')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-const { log } = require('console');
 
+app.use(express.json())
+
+app.use('/api/v1/products' ,productRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = 5000
 
-const start = async ( ) => {
+const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URL)
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, console.log(`Server is listening port ${port}...`));
     } catch (error) {
         console.log(error);
     }
